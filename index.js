815aceb3ee5ccab6c20ae70b6f2a8f26c612601e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-05-05 16:10:35 +0700

    Allow cors

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(express.static(join(__dirname, './public')));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.set('view engine', 'pug');
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', (req, res) => {
     res.render('index', { users });
 })
