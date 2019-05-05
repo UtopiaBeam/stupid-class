8675adf50745614a40cb9bfe84bc1e8b358240a9
UtopiaBeam <utopiabeam@gmail.com>
2019-05-05 15:03:43 +0700

    Fix port env

--- index.js
+++ index.js
@@ -41,4 +41,4 @@ app.post('/deleteUser/:id', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Listening to port 3000'));
